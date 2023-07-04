370ff0f95d6112b37573a9747d9890ecd59ae239
Keira Jarvis <82812348+KeiraJCoder@users.noreply.github.com>
2023-07-04 14:15:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
-let petContainer = document.querySelector("#petcontainer")
-let methodContainer = document.querySelector("#methodscontainer")
-let selectDog = document.querySelector("#dogbtn")
-let selectCat = document.querySelector("#catbtn")
-let selectSnake = document.querySelector("#snakebtn")
-let petImgs = document.querySelector("#petimgs")
-let petName = document.querySelector("#petname")
-let snakeImg = document.querySelector("#snakeimg")
-let dogImg = document.querySelector("#dogimg")
-let catImg = document.querySelector("#catimg")
+let petContainer = document.querySelector("#petcontainer");
+let methodContainer = document.querySelector("#methodscontainer");
+let selectDog = document.querySelector("#dogbtn");
+let selectCat = document.querySelector("#catbtn");
+let selectSnake = document.querySelector("#snakebtn");
+let petImgs = document.querySelector("#petimgs");
+let petName = document.querySelector("#petname");
+let snakeImg = document.querySelector("#snakeimg");
+let dogImg = document.querySelector("#dogimg");
+let catImg = document.querySelector("#catimg");
 let P;
-let progressBars = document.querySelector("#progressbars")
+let progressBars = document.querySelector("#progressbars");
+let gameOver = document.querySelector("#gameover");
 
-let gameOver = document.querySelector("#gameover")
+let giveFood = document.querySelector("#feedbtn");
+let giveDrink = document.querySelector("#drinkbtn");
+let petPlay = document.querySelector("#playbtn");
+let petSleep = document.querySelector("#sleepbtn");
+let petToilet = document.querySelector("#toiletbtn");
 
-let giveFood = document.querySelector("#feedbtn")
-let giveDrink = document.querySelector("#drinkbtn")
-let petPlay = document.querySelector("#playbtn")
-let petSleep = document.querySelector("#sleepbtn")
-let petToilet = document.querySelector("#toiletbtn")
+let hungerBar = document.querySelector("#hungerbar");
+let thirstBar = document.querySelector("#thirstbar");
+let energyBar = document.querySelector("#energybar");
+let boredomBar = document.querySelector("#boredbar");
+let toiletBar = document.querySelector("#toiletbar");
+let healthBar = document.querySelector("#healthbar");
 
-let hungerBar = document.querySelector("#hungerbar")
-let thirstBar = document.querySelector("#thirstbar")
-let energyBar = document.querySelector("#energybar")
-let boredomBar = document.querySelector("#boredbar")
-let toiletBar = document.querySelector("#toiletbar")
-let healthBar = document.querySelector("#healthbar")
-
-methodContainer.style.display = "none"
-progressBars.style.display = "none"
-gameOver.style.display = "none"
+methodContainer.style.display = "none";
+progressBars.style.display = "none";
+gameOver.style.display = "none";
 
 hungerBar.value = 100;
 thirstBar.value = 100;
@@ -37,12 +36,10 @@ boredomBar.value = 100;
 toiletBar.value = 100;
 healthBar.value = 100;
 
-// The health is calculated based on other stats
 function updateHealth() {
     healthBar.value = (hungerBar.value + thirstBar.value + energyBar.value + boredomBar.value + toiletBar.value) / 5;
     if (healthBar.value <= 0) {
         gameOver.style.display = "block";
-        // Stop all intervals
         clearInterval(hunger);
         clearInterval(thirst);
         clearInterval(energy);
@@ -74,35 +71,35 @@ let bored = setInterval(() => {
 let toilet = setInterval(() => {
     toiletBar.value--;
     updateHealth();
-}, 400)
+}, 400);
 
 giveFood.addEventListener("click", () => {
     hungerBar.value = Math.min(100, hungerBar.value + 20);
     toiletBar.value = Math.max(0, toiletBar.value - 10);
     updateHealth();
-})
+});
 
 giveDrink.addEventListener("click", () => {
     thirstBar.value = Math.min(100, thirstBar.value + 20);
     toiletBar.value = Math.max(0, toiletBar.value - 10);
     updateHealth();
-})
+});
 
 petPlay.addEventListener("click", () => {
     energyBar.value = Math.max(0, energyBar.value - 10);
     boredomBar.value = Math.min(100, boredomBar.value + 10);
     updateHealth();
-})
+});
 
-petSleep.addEventListener("click", () =>{
+petSleep.addEventListener("click", () => {
     energyBar.value = Math.min(100, energyBar.value + 20);
     updateHealth();
-})
+});
 
-petToilet.addEventListener("click", () =>{
+petToilet.addEventListener("click", () => {
     toiletBar.value = Math.min(100, toiletBar.value + 20);
     updateHealth();
-})
+});
 
 selectDog.addEventListener("click", () => {
     const name = prompt("What is your Dog's name going to be?");
@@ -112,7 +109,7 @@ selectDog.addEventListener("click", () => {
         progressBars.style.display = "block";
         petContainer.style.display = "none";
         petName.textContent = `${P.name}`;
-        P.startGame(); // Call the startGame method on the pet object
+        P.startGame();
     }
 });
 
@@ -124,7 +121,7 @@ selectSnake.addEventListener("click", () => {
         progressBars.style.display = "block";
         petContainer.style.display = "none";
         petName.textContent = `${P.name}`;
-        P.startGame(); // Call the startGame method on the pet object
+        P.startGame();
     }
 });
 
@@ -136,6 +133,6 @@ selectCat.addEventListener("click", () => {
         progressBars.style.display = "block";
         petContainer.style.display = "none";
         petName.textContent = `${P.name}`;
-        P.startGame(); // Call the startGame method on the pet object
+        P.startGame();
     }
 });
